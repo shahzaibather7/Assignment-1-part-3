c1d0a3bd1f71f68794c66bed7d931c2558c966ba
Shahzaib Ather <shahzaibather7@github.ca>
2017-07-24 17:39:05 -0700

    Initial Commit

---
+++ Assignment 1 part 3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment 1 part 3
+//
+//  Created by Mohammad Shahzaib Ather on 2017-07-24.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
